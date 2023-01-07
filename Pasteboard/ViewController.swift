c181e943b1df86e2ea018871127d8dffa802b03b
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-07 17:29:11 +0530

    implement pasteboard

--- Pasteboard/ViewController.swift
+++ Pasteboard/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+
+    let DATA = "data"
+    var pastedStrings: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let loadedStrings = UserDefaults.standard.stringArray(forKey: DATA) {
+            pastedStrings.append(contentsOf: loadedStrings)
+        }
+        showText()
     }
 
+    @IBAction func trashButton(_ sender: UIBarButtonItem) {
+        pastedStrings.removeAll()
+        UserDefaults.standard.removeObject(forKey: DATA)
+        showText()
+    }
 
+    func addText() {
+        guard let text = UIPasteboard.general.string, !pastedStrings.contains(text) else {
+            return
+        }
+        pastedStrings.append(text)
+        UserDefaults.standard.set(pastedStrings, forKey: DATA)
+        showText()
+    }
+
+    func showText() {
+        textView.text = ""
+        for str in pastedStrings {
+            textView.text.append("\(str)\n\n")
+        }
+    }
 }
-
